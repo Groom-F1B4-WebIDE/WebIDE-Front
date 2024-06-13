08e1faea58368c42e960e7f73d856dd29fe6b49c
ByeongJun-Jang <qudwns8616@gmail.com>
2024-06-13 15:12:12 +0900

    update directory and mainpage

--- READEME.md
+++
@@ -1,1 +0,0 @@
-# F1B4 WebIDE-Front
--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Main from './pages/Main';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleToggle = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? 'app dark-mode' : 'app'}>
+      <Main darkMode={darkMode} handleToggle={handleToggle} />
     </div>
   );
 }
