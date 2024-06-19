8423f6331c73129aaf27fc2d8ef107e2b26b94c7
ByeongJun-Jang <qudwns8616@gmail.com>
2024-06-19 13:04:23 +0900

    update login and register page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Main from './pages/Main';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 import CodeCompiler from './pages/CodeCompiler';
 
 function App() {
@@ -15,6 +18,9 @@ function App() {
     <Router>
       <div className={darkMode ? 'app dark-mode' : 'app'}>
         <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/code" element={<CodeCompiler />} />
           <Route
             path="/"
